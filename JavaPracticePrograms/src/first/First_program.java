c59c1363843b57e92f9b1c634164c8ddf5ec2395
HP <HP@DESKTOP-IF3PL1C>
2023-02-22 20:05:24 -0800

    My first upload

---
+++ JavaPracticePrograms/src/first/First_program.java
@@ -0,0 +1,11 @@
+package first;
+
+public class First_program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("This is my java programs");
+
+	}
+
+}
---
+++ JavaPracticePrograms/src/first/Variables.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int no = 22;
+		String name = "Sujitha ";
+		System.out.println("Hi this is "+name +"iam " +no);
+
+	}
+
+}
