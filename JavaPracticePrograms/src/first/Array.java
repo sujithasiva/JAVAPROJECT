604230fbbce5eb04110928a4b5a0b0c5c5644f17
HP <HP@DESKTOP-IF3PL1C>
2023-04-03 13:03:43 +0530

    array

---
+++ JavaPracticePrograms/src/first/Array.java
@@ -0,0 +1,17 @@
+package first;
+
+public class Array {
+
+	public static void main(String[] args) {
+		String[] name= {"vikram","amar","rolex","santa"};
+		{
+			System.out.println(name[2]);
+			System.out.println(name.length);
+			for(String i:name) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
